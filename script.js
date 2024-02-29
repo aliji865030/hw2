c7c8d8bf766da645ef5d00ec89f04766fedce590
aliji865030 <82137372+aliji865030@users.noreply.github.com>
2024-02-29 15:37:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,48 @@
-function Randerdata() {
-  return (
-    <div class="container">
-      <h1>Topics covered</h1>
-      <p>
-        The following is a list of all the topics we cover in the MDN learning
-        area.
-      </p>
-      <p>
-        <a href="https://legacy.reactjs.org/docs/getting-started.html">
-          Getting started with the web
-        </a>
-      </p>
-      <p class="left">
-        Provides a practical introduction to web development for complete
-        beginners.
-      </p>
-      <p>
-        <a href="https://developer.mozilla.org/en-US/docs/Learn/HTML">
-          HTML-Structuring the web
-        </a>
-      </p>
-      <p class="left">
-        HTML is the language that we use to structure the different parts of our
-        content and define what their meaning or purpose is. This topic teaches
-        HTML in detail.
-      </p>
-      <p>
-        <a href="https://developer.mozilla.org/en-US/docs/Web/CSS">
-          CSS-Styling the web
-        </a>
-      </p>
-      <p class="left">
-        CSS is the language that we use to control our web content's style and
-        layout, as well as adding behavior like animation. This topic provides
-        comprehensive coverage of CSS.
-      </p>
-    </div>
-  );
-}
+const heading = React.createElement("h1", {}, "Topics covered");
+const paraFirst = React.createElement(
+  "p",
+  {},
+  "The following is a list of all the topics we cover in the MDN learning area."
+);
+const aFirst = React.createElement(
+  "a",
+  { href: "https://legacy.reactjs.org/docs/getting-started.html" },
+  "Getting started with the web"
+);
+const paraSecond = React.createElement(
+  "p",
+  { style: { marginLeft: "20px" } },
+  "Provides a practical introduction to web development for complete beginners."
+);
+const aSecond = React.createElement(
+  "a",
+  { href: "https://developer.mozilla.org/en-US/docs/Learn/HTML" },
+  "HTML-Structuring the web"
+);
+const paraThird = React.createElement(
+  "p",
+  { style: { marginLeft: "20px" } },
+  `HTML is the language that we use to structure the different parts of our content and define what their meaning or purpose is. This topic teaches HTML in detail.`
+);
+const aThird = React.createElement(
+  "a",
+  { href: "https://developer.mozilla.org/en-US/docs/Web/CSS" },
+  "CSS-Styling the web"
+);
+const paraForth = React.createElement(
+  "p",
+  { style: { marginLeft: "20px" } },
+  `CSS is the language that we use to control our web content's style and layout, as well as adding behavior like animation. This topic provides comprehensive coverage of CSS.`
+);
 
-ReactDOM.render(<Randerdata />, document.querySelector("#root"));
+const DIV = React.createElement("div", { className: "content" }, [
+  heading,
+  paraFirst,
+  aFirst,
+  paraSecond,
+  aSecond,
+  paraThird,
+  aThird,
+  paraForth,
+]);
+ReactDOM.render(DIV, document.querySelector("#root"));
